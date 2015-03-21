cf1e28b83564b95bfedf860373fa17ec2f077574
Adam Pash <adam.pash@gmail.com>
2015-03-21 12:04:02 -0400

    Dashboard stylin'

--- app/assets/javascripts/nav.coffee
+++ app/assets/javascripts/nav.coffee
@@ -2,5 +2,13 @@ ready = ->
   $('nav .menu').on 'click', ->
     $('nav .dropdown').toggle()
 
+  $(document).on 'click', ->
+    target_class = event.target.className
+    unless target_class is "fa fa-bars fa-lg menu"
+      closest = $(event.target).closest('.dropdown')
+      if closest.length is 0
+        $('nav .dropdown').hide()
+
+
 $(document).ready(ready)
 $(document).on('page:load', ready)
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,17 @@
 module PostsHelper
   def site_header(site_name)
-    name = site_name.nil? ? "Gawker Reader" : site_name.upcase
+    name = site_name.nil? ? "The Gawker Reader" : site_name.upcase
     url = site_name.nil? ? dashboard_path : "/#{site_name}"
     raw "<h1 class=\"site\">#{ link_to name, url }</h1>"
+  end
+
+  def timestamp(time, opts={})
+    opts[:short] = opts[:short] || false
+    if opts[:short]
+      strf = "%-m/%-d/%Y"
+    else
+      strf = "%-m/%-d/%Y %-l:%M%P"
+    end
+    raw "<span class=\"timestamp\">#{time.in_time_zone("Eastern Time (US & Canada)").strftime(strf)}</span>"
   end
 end
