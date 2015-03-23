345c92b4d366671ca8ad5cd104b584cee3dcbbd9
Adam Pash <adam.pash@gmail.com>
2015-03-23 12:58:57 -0400

    bugfix more

--- lib/slack_notifier.rb
+++ lib/slack_notifier.rb
@@ -3,7 +3,7 @@ SLACK_WEBHOOK = ENV["SLACK_WEBHOOK"]
 
 module SlackNotifier
   def self.notify(message, channel="@adampash")
-    notifier = Slack::Notifier.new SLACK_WEBHOOK, {"unfurl_links": false}
+    notifier = Slack::Notifier.new SLACK_WEBHOOK, {unfurl_links: false}
 
     notifier.ping message,
       icon_emoji: ":baby_symbol:",
